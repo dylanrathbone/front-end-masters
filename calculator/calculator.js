60078f35472acae95ae4846eea894ce932bb674b
Dylan <dylanrathbone@gmail.come>
2018-11-08 05:53:27 +1000

    add initial js file

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,12 +1,3 @@
-function addZero() {
-    const lcd_display = document.querySelector('.lcd-display');
-        lcd_display.value = 'blaa';
-        console.log("add zero to display")
-}   
-
-
-function zeroDisplay() {
-    const lcd_display = document.querySelector('.lcd-display');
-        lcd_display.value = '0';
-        console.log("display zeroed");
-}
+document.querySelector('.calculator').addEventListener('click', function(event) {
+    console.log(`You clicked on button ${event.target.innerText}`);
+});
