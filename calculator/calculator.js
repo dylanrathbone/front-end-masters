a4186246ef2bd8ae47bfff7637e6a8a82641d8a9
Dylan <dylanrathbone@gmail.come>
2018-11-07 21:56:12 +1000

    remove padding from html

---
+++ calculator/calculator.js
@@ -0,0 +1,12 @@
+function addZero() {
+    const lcd_display = document.querySelector('.lcd-display');
+        lcd_display.value = 'blaa';
+        console.log("add zero to display")
+}   
+
+
+function zeroDisplay() {
+    const lcd_display = document.querySelector('.lcd-display');
+        lcd_display.value = '0';
+        console.log("display zeroed");
+}
