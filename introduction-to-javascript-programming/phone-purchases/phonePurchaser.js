dc2fadd8e2c4a1bd3973d6fb00a7f8139e7bf2d1
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2018-11-15 05:46:19 +1000

    Refactored code

--- introduction-to-javascript-programming/phone-purchases/phonePurchaser.js
+++ introduction-to-javascript-programming/phone-purchases/phonePurchaser.js
@@ -1,5 +1,6 @@
 let bankBalance = 0;
 let spendingThreshold = 0;
+let totalSpent = 0;
 const taxRate = 12.5;
 const phonePrice = 1250.00;
 const accessoryPrice = 50.00;
@@ -13,23 +14,49 @@ function setSpendingThreshold() {
 }
 
 function getBankBalance() {
-    return bankBalance;
+    return parseFloat(bankBalance).toFixed(2);
 }
 
 function getSpendingThreshold() {
     return spendingThreshold;
 }
 
+function calculateSubTotal() {
+    return phonePrice + accessoryPrice;
+}
+
+function calculateTax() {
+    return (taxRate / 100) * calculateSubTotal();
+}
+
 function calculateTotal() {
-    let subTotal = phonePrice + accessoryPrice;
-    let total = subTotal * taxRate / 100;
-    return total;
+    return parseFloat(calculateSubTotal() + calculateTax()).toFixed(2);
+}
+
+function buyAPhone() {
+    totalSpent += calculateSubTotal();
+    return bankBalance -= calculateSubTotal();
+}
+
+function letsBuyPhones() {
+    let phoneCounter = 0;
+    while(totalSpent <= parseFloat(getSpendingThreshold())) {
+        console.log(totalSpent);
+        console.log(parseFloat(getSpendingThreshold()));
+        console.log(totalSpent + parseFloat(getSpendingThreshold()));
+        console.log(parseFloat(getBankBalance());
+        if(parseFloat(getBankBalance()) >= parseFloat(calculateTotal())) {
+            console.log(`Buying phone at: ${calculateTotal()} Balance is: ${getBankBalance()} `)
+            buyAPhone();
+            phoneCounter++;
+        } else {
+            console.log(`Bad luck, you do not have enough money for this phone. Balance is ${getBankBalance()} phone price ${calculateTotal()}`)
+            break;
+        }
+    }
+    console.log(`Balance exhausted. Phone bought ${phoneCounter}`);
 }
 
 setBankBalance();
 setSpendingThreshold();
-
-
-console.log(`Your bank balance is: ${getBankBalance()}`);
-console.log(`Your spending threshold is: ${getSpendingThreshold()}`);
-console.log(`total price is: ${calculateTotal()}`);
+letsBuyPhones();
