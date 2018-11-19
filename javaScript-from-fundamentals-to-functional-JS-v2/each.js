28918e0dc62efa90b6c3c3607e874004dd8d027d
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2018-11-20 05:47:52 +1000

    Initial commit for JS Fundamentals course

---
+++ javaScript-from-fundamentals-to-functional-JS-v2/each.js
@@ -0,0 +1,24 @@
+const _ = {};
+
+/*
+ Define _.each
+*/
+_.each = function(list, callback) {
+    if(Array.isArray(list)) {
+        for(let i = 0; i < list.length; i++) {
+            callback(list[i], i, list)
+        }
+    } else {
+
+    }
+}
+/*
+ Call ._each
+*/
+_.each(['Sally', 'John', 'Peter'], function(name, i, list) {
+    if(list[i + 1]) {
+        console.log(name, 'is younger than', list[i] + 1);
+    } else {
+        console.log(name, 'is the oldest');
+    }
+});
