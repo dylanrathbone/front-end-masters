d8ecd9b1c59fbacfdc214aac4203dc710429e740
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-02-05 05:58:29 +1000

    Update comments

--- javaScript-the-hard-parts/exercise_1/callBacksAndHigherOrderFunctions.js
+++ javaScript-the-hard-parts/exercise_1/callBacksAndHigherOrderFunctions.js
@@ -1,7 +1,7 @@
 /*  
-Challenge 1
-Create a function addTwo that accepts one input and adds 2 to it.
-*/
+ * Challenge 1
+ * Create a function addTwo that accepts one input and adds 2 to it.
+ */
 function addTwo(number) {
     return number + 2;
 }
@@ -9,9 +9,9 @@ console.log(addTwo(2));
 console.log(addTwo(10));
 
 /*
-Challenge 2
-Create a function addS that accepts one input and adds an "s" to it.
-*/
+ * Challenge 2
+ * Create a function addS that accepts one input and adds an "s" to it.
+ */
 function addS(input) {
     return input + 's';
 }
@@ -19,9 +19,9 @@ console.log(addS('pizza'));
 console.log(addS('bagel'));
 
 /*
-Challenge 4
-Create a function multiplyByTwo that accepts one number and multiplies it by 2
-*/
+ * Challenge 4
+ * Create a function multiplyByTwo that accepts one number and multiplies it by 2
+ */
 function multiplyByTwo(num) {
     return num * 2;
 }
@@ -32,8 +32,11 @@ console.log(multiplyByTwo(5));
 Challenge 3
 Create a function called map that takes two inputs:
 1. an array of numbers (a list of numbers)
-2. a 'callback' function - a function that is applied to each element of the array (inside of the function 'map')
-Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+2. a 'callback' function - a function that is applied 
+to each element of the array (inside of the function 'map')
+Have map return a new array filled with numbers that 
+are the result of using the 'callback' function on each 
+element of the input array.
 */
 function map(list, callback) {
     let buffer = [];
@@ -48,7 +51,9 @@ console.log(map([2, 3, 4, 5], multiplyByTwo));
 
 /*
 Challenge 4
-The function forEach takes an array and a callback, and runs the callback on each element of the array. forEach does not return anything.
+The function forEach takes an array and a callback, 
+and runs the callback on each element of the array. 
+forEach does not return anything.
 */
 function forEach(array, callback) {
     for(let element in array) {
@@ -65,5 +70,7 @@ console.log(`Calling forEach: ${alphabet}`);
 
 /*
 Extension 2
-The function reduce takes an array and reduces the elements to a single value. For example it can sum all the numbers, multiply them, or any operation that you can put into a function.
+The function reduce takes an array and reduces the elements 
+to a single value. For example it can sum all the numbers, 
+multiply them, or any operation that you can put into a function.
 */
