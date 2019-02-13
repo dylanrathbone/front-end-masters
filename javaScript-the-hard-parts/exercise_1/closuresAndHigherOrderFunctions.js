549e7145f9f5cd9bbbc82844de313068f37955ee
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-02-14 05:52:13 +1000

    Add challenge 4

--- javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
+++ javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
@@ -56,13 +56,51 @@
   var willCounter = outer();
   var jasCounter = outer();
 
-  // Uncomment each of these lines one by one.
+// Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
-
-
 willCounter(); //Will return 1
 willCounter(); //Will return 2
 willCounter(); //Will return 3
 
 jasCounter();  //Will return 1;
 willCounter(); //Will return 4;
+
+
+/*
+ * Challenge 4
+ * Now we are going to create a function addByX 
+ * that returns a function that will add an input 
+ * by x.
+ */
+function addByX(adderVal) {
+    function addIt(number) {
+        console.log(number + adderVal);
+    }
+    return addIt;
+}
+
+var addByTwo = addByX(2)
+addByTwo(1); //should return 3
+addByTwo(2); //should return 4
+addByTwo(3); //should return 5
+
+var addByThree = addByX(3);
+addByThree(1); //should return 4
+addByThree(2); //should return 5
+
+var addByFour = addByX(4);
+addByFour(4); //should return 8
+addByFour(10); //should return 14
+
+
+
+/*
+ * Extension: Challenge 5
+ * Write a function once that accepts a callback 
+ * as input and returns a function. When the returned 
+ * function is called the first time, it should call 
+ * the callback and return that output. If it is called 
+ * any additional times, instead of calling the callback 
+ * again it will simply return the output value from the 
+ * first time it was called.
+*/
