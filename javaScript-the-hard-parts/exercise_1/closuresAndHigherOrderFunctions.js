ce47635c492c7264a23aff29fed900a1726e33aa
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-02-14 05:30:51 +1000

    Initial commit for closure exercises

--- javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
+++ javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
@@ -5,7 +5,6 @@
  * and returns a function. When that created 
  * function is called, it should print "hello".
  */
-
  function createFunction() {
      function sayHello() {
          console.log("Hello!")
