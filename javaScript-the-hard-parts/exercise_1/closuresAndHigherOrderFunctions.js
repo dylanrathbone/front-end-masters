d1465fc7449dcae70804e66e84a121b71f4803cb
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-02-14 05:42:51 +1000

    Add challenge 4

--- javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
+++ javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
@@ -36,3 +36,33 @@
   callPrinter("Closures are interesting...");
 
 
+  /*
+   * Challenge 3
+   * Examine the code for the outer function. 
+   * Notice that we are returning a function and that 
+   * function is using variables that are outside of 
+   * its scope. Uncomment those lines of code. Try to 
+   * deduce the output before executing.
+   */
+  function outer() {
+    var counter = 0; // this variable is outside incrementCounter's scope
+    function incrementCounter () {
+      counter ++;
+      console.log('counter', counter);
+    }
+    return incrementCounter;
+  }
+  
+  var willCounter = outer();
+  var jasCounter = outer();
+
+  // Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+
+willCounter(); //Will return 1
+willCounter(); //Will return 2
+willCounter(); //Will return 3
+
+jasCounter();  //Will return 1;
+willCounter(); //Will return 4;
