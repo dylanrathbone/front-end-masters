7b46e87a437546f2bfc873ec7105a11ac50773cd
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-02-14 05:37:53 +1000

    Add challenge 2

--- javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
+++ javaScript-the-hard-parts/exercise_1/closuresAndHigherOrderFunctions.js
@@ -1,6 +1,6 @@
 
  /*
- * Extension 1
+ * Challenge 1
  * Create a function createFunction that creates 
  * and returns a function. When that created 
  * function is called, it should print "hello".
@@ -12,6 +12,27 @@
      return sayHello;
  }
 
-
  let hello = createFunction();
  hello();
+
+ /*
+  * Challenge 2
+  * Create a function createFunctionPrinter 
+  * that accepts one input and returns a function. 
+  * When that created  function is called, it should 
+  * print out the input that was used when the function 
+  * was created.
+  */
+
+
+  function createFunctionPrinter(){
+      function printInput(input) {
+          console.log(`This was your input: ${input}`);
+      }
+      return printInput;
+  }
+
+  let callPrinter = createFunctionPrinter();
+  callPrinter("Closures are interesting...");
+
+
