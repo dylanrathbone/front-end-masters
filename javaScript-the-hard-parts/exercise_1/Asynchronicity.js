80c447f5208a6f44a8f17deac93d26b2eca30006
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-04-30 05:59:25 +1000

    Added code for asynchronicity challenges

---
+++ javaScript-the-hard-parts/exercise_1/Asynchronicity.js
@@ -0,0 +1,46 @@
+/////////////////
+//             //
+// CHALLENGE 1 //
+//             //
+/////////////////
+
+/* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 1');
+console.log('I am at the beginning of the code');
+setTimeout(logIt, 3);
+
+console.log('I am at the end of the code');
+console.log('End of Challenge 1');
+
+function logIt() {
+    console.log("I am in the setTimeout callback function");
+}
+// */// (do not alter this line)
+
+
+/////////////////
+//             //
+// CHALLENGE 2 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 2');
+
+console.log('Start of Challenge 2');
+
+setInterval(logIt, 2000);
+
+function logIt() {
+    console.log("Interval Hello!");
+}
+
+function clearAllIntervals() {
+  for (let i = 0; i < 1000; i++) {
+    clearInterval(i);
+  }
+}
+console.log('End of Challenge 2');
+// */// (do not alter this line)
+
+
