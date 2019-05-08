b97f316bcfda2950389753e1e03b2cae8d824931
Dylan <dylanrathbone+github_eat-regional-heave_6@gmail.com>
2019-05-09 05:56:45 +1000

    Updates

--- javaScript-the-hard-parts/exercise_1/prototypes.js
+++ javaScript-the-hard-parts/exercise_1/prototypes.js
@@ -34,3 +34,45 @@ huskey.makeBark()
 cocker.getAge();
 cocker.getBreed();
 cocker.makeBark();
+
+
+/*
+ * Above re-written for modern JS. We use a style here
+ * similar to other OO languages. What is happening here 
+ * is exactly the same as above, only it could be considered
+ * more readable and better practice
+ */
+
+class Dog {
+    constructor(breed, age) {
+        this.breed = breed;
+        this.age = age;
+    }
+
+    getBreed() {
+        console.log(`Dog breed is: ${this.breed}`)
+    }
+
+    getAge() {
+        console.log(`Dog age is: ${this.age}`)
+    }
+
+    makeBark = function() {
+        if(this.breed == "Huskey") {
+            console.log("Woof! Woof")
+        } else {
+            console.log("Bow wow")
+        }
+    }
+}
+
+let greatDane = new Dog("Great Dane", "12");
+let irishWolfHound = new Dog("Irish Wolf Hound", "2")
+
+greatDane.getAge();
+greatDane.getBreed();
+greatDane.makeBark()
+
+irishWolfHound.getAge();
+irishWolfHound.getBreed();
+irishWolfHound.makeBark();
